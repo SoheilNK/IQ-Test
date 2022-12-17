f8f94a1de802584b7e95cf2a70b9dad3ffefc992
Soheil Najmabadi Kia <sohnajm@gmail.com>
2022-12-17 14:56:29 -0330

    data validation

---
+++ IQ-Test/sec-01-q01.js
@@ -0,0 +1,17 @@
+function checkinput() {
+    // Get the value of the input field with id="numb"
+    let x = document.getElementById("q01").value;
+    points = JSON.parse(localStorage.getItem("score"));
+    // If x is Not a positive Number
+    let text;
+    if (isNaN(x) || x < 0) {
+        text = "Enter a positive number";
+        points = points - 1;
+        localStorage.setItem("score", JSON.stringify(points));
+    } else {
+        text = "Input OK";
+        points = points + 1;
+        localStorage.setItem("score", JSON.stringify(points));
+    }
+  document.getElementById("alert").innerHTML = text + "  " + points;
+}
