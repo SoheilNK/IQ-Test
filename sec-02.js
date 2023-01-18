4f7c73c9a9d6ab0b681423126215c7d8b7ae4a7b
Soheil Najmabadi Kia <sohnajm@gmail.com>
2023-01-18 09:46:07 -0330

    for merge

--- sec-02.js
+++ sec-02.js
@@ -121,46 +121,12 @@ const sec2 = [
   },
 ];
 //------------------------------drag and drop----------------------
-// function allowDrop(ev) {
-//   ev.preventDefault();
-// }
-
-// function drag(ev) {
-//   ev.dataTransfer.setData("text", ev.target.id);
-//   ev.dataTransfer.setData("source", ev.path[1].id);
-// }
-
-// function drop(ev) {
-//   ev.preventDefault();
-//   var data = ev.dataTransfer.getData("text");
-//   var target_id = ev.target.id;
-//   var source_id = ev.dataTransfer.getData("source");
-//   imagefile = document.getElementById(source_id).innerHTML;
-//   ev.target.appendChild(document.getElementById(data));
-//   document
-//     .getElementById(source_id)
-//     .setAttribute("ondragover", "allowDrop(event)");
-//   ev.target.setAttribute("ondragover", "");
-//   if (source_id.slice(0, 4) == "cube") {
-//     idName = data.slice(0, 4);
-//     idNumber = +data.slice(5);
-//     idNumber++;
-//     newId = idName + "-" + idNumber;
-//     document.getElementById(source_id).innerHTML = imagefile;
-//     document.getElementById(data).setAttribute("id", newId);
-//     document.getElementById(source_id).setAttribute("ondragover", "");
-//   }
-// }
 
 
 function dragStart(e) {
   e.dataTransfer.setData("data", e.target.id);
-  // e.dataTranfser.setData("source", e.target.parentElement.id);
     e.dataTransfer.setData("source", e.target.parentElement.id);
 
-    // setTimeout(() => {
-    //     e.target.classList.add('hide');
-    // }, 0);
 }
 
 
